4409375170ab2c4030905a9bc55928bdd35c1b6f
Ivan De wit <deWit.Ivan777@gmail.com>
2020-07-09 12:00:46 +0200

    Add project files.

---
+++ eBlocksWeb/Vue-Scripts/Category/category.vue
@@ -0,0 +1,44 @@
+﻿<template>
+    <div id="category">
+    <h1>Categories</h1>
+ <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"></v-data-table>
+    </div>
+</template>
+
+
+    <script>
+        export default {
+            name: "category-component",
+        data(){
+        return{
+        headers:[
+        {
+         text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+        },
+            { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts:[
+        {
+           name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+        },
+        ],
+        }
+        },
+    };
+</script>
---
+++ eBlocksWeb/Vue-Scripts/Category/index.js
@@ -0,0 +1,14 @@
+﻿import Vue from 'vue';
+import Vuetify from 'vuetify'
+import CategoryComponent from './category.vue';
+
+Vue.use(Vuetify);
+Vue.config.devtools = true;
+
+let vue = new Vue({
+    el: "#app",
+    vuetify: new Vuetify(),
+    components: {
+        CategoryComponent
+    }
+});
